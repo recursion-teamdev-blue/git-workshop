f58db32da509a9e4f28b9bf7e3725b0edfd147ba
TakTadano <ms6rx7861@gmail.com>
2022-09-26 21:12:10 +0900

    slider改修

--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,11 @@ const swiper = new Swiper('.slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    speed: 2000, // 何秒かけてスライドを切り替える（ミリ秒:1000=1秒）
-    effect: 'fade', // ふわっとアニメーションさせてスライドを切り替える
+    speed: 2000, // 何秒かけてスライドを切り替える（ミリ秒）
+    effect: 'fade', // フェードでスライドを切り替え
 
     autoplay: {
-        delay: 5000 // 次のスライドに切り替わる時間の設定（ミリ秒:1000=1秒）
+        delay: 3000 // 次のスライドに切り替わる時間の設定（ミリ秒）
     },
   
     // If we need pagination
